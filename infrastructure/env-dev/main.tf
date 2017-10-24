ec3a93271b19f62f98d6004306c94f6c59cc3b7e
Derek Lieu <dl@dereklieu.com>
2017-10-23 21:17:10 -0400

    Simplify structure, add variables file to app

--- infrastructure/env-dev/main.tf
+++ infrastructure/env-dev/main.tf
@@ -4,12 +4,12 @@ variable "aws_access_key"       {}
 variable "aws_secret_key"       {}
 
 provider "aws" {
-  region        = "${var.region}"
-  access_key    = "${var.aws_access_key}"
-  secret_key    = "${var.aws_secret_key}"
+  region     = "${var.region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 module "app" {
-  source = "../modules/app"
-  domain          = "${var.domain}"
+  source     = "../app"
+  domain     = "${var.domain}"
 }
