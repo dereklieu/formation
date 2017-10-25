bc2c2c36f2badc78d4d1afe5cf61df7ef4421553
Derek Lieu <dl@dereklieu.com>
2017-10-25 10:31:11 -0400

    Export to default

--- lambda/handler.js
+++ lambda/handler.js
@@ -9,7 +9,7 @@ const gzip = promisify(zlib.gzip)
 const s3 = new AWS.S3()
 const cloudfront = new AWS.CloudFront()
 
-module.exports = function handler (event, context, callback) {
+module.exports.default = function handler (event, context, callback) {
   run()
   .then(result => callback(null, result))
   .catch(err => callback(err))
@@ -21,7 +21,7 @@ function run () {
   .then(invalidate)
 }
 
-async function upload (filename) {
+function upload (filename) {
   const absolutePath = path.join(__dirname, 'app', filename)
   const ext = path.extname(filename)
   const expiresIn = ext === '.html' ? 900 : 7200
@@ -40,7 +40,7 @@ async function upload (filename) {
   })
 }
 
-async function invalidate () {
+function invalidate () {
   const params = {
     DistributionId: process.env.CF_DISTRIBUTION,
     InvalidationBatch: {
