0dd8d3eec3cb2e6371ae4acb00b9e4075c6e9a43
Derek Lieu <dl@dereklieu.com>
2019-01-29 21:51:25 -0800

    Homepage styles

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,29 @@
 'use strict'
+require('../sass/home.scss')
+
 const React = require('react')
+const c = require('classnames')
+
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isBlur: false
+    }
+    setTimeout(() => this.setState({ isBlur: true }), 800)
+  }
+
   render () {
     const {imageCredit} = this.props
     return (
-      <main className='spread image-canvas'>
-        <aside className='image-credit'>{imageCredit}</aside>
-      </main>
+      <div className='full noscroll'>
+        <div className={c('full spread image-canvas transition-filter', {blur: this.state.isBlur})} />
+
+        <main className='over-spread'>
+          <aside className='image-credit'>{imageCredit}</aside>
+        </main>
+
+      </div>
     )
   }
 }
