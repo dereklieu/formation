84b40dbf80bde748d37c241afc676f9814f104fe
Derek Lieu <dl@dereklieu.com>
2019-02-11 21:05:09 -0800

    Create greeting assets

--- app/collect.js
+++ app/collect.js
@@ -3,22 +3,33 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 const mime = require('mime')
 
+function random (array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 // Loads an image registry and randomly selects an image from it.
 // Returns the image path and credit.
 async function collect () {
-  const registryPath = path.join(__dirname, 'assets/registry.yaml')
+  const assetsPath = path.join(__dirname, 'assets/')
   try {
-    var registry = yaml.safeLoad(fs.readFileSync(registryPath))
+    var registry = yaml.safeLoad(fs.readFileSync(path.join(assetsPath, 'registry.yaml')))
   } catch (e) {
     throw new Error(e)
   }
-  const { images } = registry
-  const image = images[Math.floor(Math.random() * images.length)]
+  const image = random(registry.images)
   const imagePath = path.join(__dirname, 'assets/images', image.file)
+
+  try {
+    var greetings = yaml.safeLoad(fs.readFileSync(path.join(assetsPath, 'greeting.yaml')))
+  } catch (e) {
+      throw new Error(e)
+  }
+  const greeting = random(greetings.greetings)
 
   return {
     imageCredit: image.credit,
-    imageFile: image.file
+    imageFile: image.file,
+    greeting
   }
 }
 module.exports = collect
