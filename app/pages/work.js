f7c707a72bf76611af838ee6f28e507bf0db3f5f
Derek Lieu <dl@dereklieu.com>
2019-03-09 16:52:43 -0800

    Bare minimal work page

--- app/pages/work.js
+++ app/pages/work.js
@@ -1,10 +1,34 @@
 'use strict'
 
 const React = require('react')
+const { Helmet } = require('react-helmet')
+const { link } = require('../utils')
 
 class Work extends React.Component {
   render () {
-    return <div>Work!</div>
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>Work</title>
+        </Helmet>
+        <div className='bg-light'>
+          <main className='main prose'>
+            <h3 className='main-title bg-dark inline-wrap'>Good Work</h3>
+            <section className='section'>
+              <p>I write software for journalists, humanitarian crises mappers and responders, and people doing international development.</p>
+              <p>The best part of my job is getting to meet face to face with someone in one of these fields, and learning enough so as not to make some terrible design choice that would hinder or otherwise completely wreck every aspect of their important, live-saving, democracy-affirming work.</p>
+            </section>
+
+            <section className='section'>
+              <p>I'm currently building a platform to dedupe relief efforts in the wake of a disaster by allowing crises responders to share actionable data and analysis. Situation reports as we know them today are hard to follow, and are out of date before they're published. I'm working with a group called <a target='_blank' href='https://nethope.org/'>NetHope</a> to do this better.</p>
+              <p>(If you work with situation reports, I'd love to hear from you. You can also follow the work on <a href='https://github.com/developmentseed/nethope-sitrep-frontend' target='_blank'>Github</a>.)</p>
+              <p>In the past few years I've led software projects in partnership with the Washington Post, various departments in the World Bank, and the Federation of Red Cross Societies (IFRC). You can find my resume <a href={link('resume')} title='Resume'>here</a>.</p>
+            </section>
+
+          </main>
+        </div>
+      </React.Fragment>
+    )
   }
 }
 
