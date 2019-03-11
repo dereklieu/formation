0610459a1ece8bcaa9dd85691a49d7be0e21b8d2
Derek Lieu <dl@dereklieu.com>
2019-03-11 10:41:22 -0700

    Fill out resume

--- app/pages/resume.js
+++ app/pages/resume.js
@@ -21,30 +21,50 @@ class Resume extends React.Component {
           <p>I currently work at <a href='https://developmentseed.org/' target='_blank'>Development Seed</a>.</p>
         </section>
 
-        <section className='section' id='jobs'>
-          <div className='resume-split'>
-            <h3 className='section-title'>Jobs</h3>
-            <dl className='dl resume-dl'>
-              <dt><em>From January 2014 to present</em></dt>
-              <dd>Development Seed <b>Senior Software Engineer/Product Lead</b> <em>Washington, DC</em></dd>
-              <dt>From October 2010 to January 2014</dt>
-              <dd>Chronicle of Philanthropy <b>Graphics Journalist</b> <em>Washington, DC</em></dd>
-              <dt>From June to Sept, 2010</dt>
-              <dd>Audrey <b>Web Production Intern</b> <em>Los Angeles, CA</em></dd>
-            </dl>
-            <h3 className='section-title'>Work</h3>
-            <dl className='dl resume-dl'>
-              <dt><em>From January 2014 to present</em></dt>
-              <dd>Development Seed <b>Senior Software Engineer/Product Lead</b> <em>Washington, DC</em></dd>
-              <dt>From October 2010 to January 2014</dt>
-              <dd>Chronicle of Philanthropy <b>Graphics Journalist</b> <em>Washington, DC</em></dd>
-              <dt>From June to Sept, 2010</dt>
-              <dd>Audrey <b>Web Production Intern</b> <em>Los Angeles, CA</em></dd>
-            </dl>
-          </div>
+        <section className='section section-resume' id='jobs'>
+          <h3 className='section-title'>Jobs</h3>
+          <dl className='resume-detail'>
+            <dt><em>From January 2014 to present</em></dt>
+            <dd>Development Seed <b>Senior Software Engineer/Product Lead</b> <em>Washington, DC</em></dd>
+            <dt>From October 2010 to January 2014</dt>
+            <dd>Chronicle of Philanthropy <b>Graphics Journalist</b> <em>Washington, DC</em></dd>
+            <dt>From June to Sept, 2010</dt>
+            <dd>Audrey <b>Web Production Intern</b> <em>Los Angeles, CA</em></dd>
+          </dl>
         </section>
 
-        <section className='section' id='work'>
+        <section className='section section-resume' id='skills'>
+          <h3 className='section-title'>Skills</h3>
+          <ul className='resume-detail'>
+            <li>Technical design leadership</li>
+            <li>User research, wireframing, and information design</li>
+            <li>Javascript, HTML, CSS, Python, and Bash programming environments</li>
+            <li>Web component rendering and MVC libraries e.g. React, Django</li>
+            <li>Client-side vector/GIS rendering libraries e.g. D3, MapboxGl</li>
+            <li>REST concepts, API design and implementation, Postgres</li>
+            <li>Serverless infrastructure concepts and implementation in AWS, Azure</li>
+          </ul>
+        </section>
+
+        <section className='section section-resume' id='work'>
+          <h3 className='section-title'>Recent Work</h3>
+          <dl className='resume-detail'>
+            <dt><b>Washington Post 2016 Elections Map</b></dt>
+            <dd>
+              <ul>
+                <li>Led a small team to engineer the elections</li>
+                <li>Didn't get Marco Rubio elected</li>
+              </ul>
+            </dd>
+          </dl>
+        </section>
+
+        <section className='section section-resume' id='misc'>
+          <h3 className='section-title'>Misc</h3>
+          <dl className='resume-detail'>
+            <dt><strong>Bachelors of Arts, UCLA 2009</strong></dt>
+            <dd>My grades were ok</dd>
+          </dl>
         </section>
       </main>
     )
