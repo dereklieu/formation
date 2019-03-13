dd343d6877896459b0b09622df81ca6c0598f68c
Derek Lieu <dl@dereklieu.com>
2019-03-12 21:55:58 -0700

    Finish out resume

--- app/pages/resume.js
+++ app/pages/resume.js
@@ -15,7 +15,7 @@ class Resume extends React.Component {
         </Helmet>
         <main className='main'>
           <section className='prose' id='greetings'>
-            <h3 className='main-title bg-dark inline-wrap'>Hello</h3>
+            <h3 className='main-title bg-dark inline-wrap'>Hay</h3>
           </section>
 
           <section className='section prose' id='brief'>
@@ -43,23 +43,23 @@ class Resume extends React.Component {
               <li>User research, wireframing, and information design</li>
               <li>Javascript, HTML, CSS, Python, and Bash programming environments</li>
               <li>Web component rendering and MVC libraries e.g. React, Django</li>
-              <li>Client-side vector/GIS rendering libraries e.g. D3, MapboxGl</li>
+              <li>Client-side vector/GIS rendering libraries e.g. D3, Mapbox GL</li>
               <li>REST concepts, API design and implementation, Postgres</li>
               <li>Serverless infrastructure concepts and implementation in AWS, Azure</li>
             </ul>
           </section>
 
           <section className='section section-resume' id='recent-projects'>
-            <h3 className='section-title'>Recent Projects</h3>
+            <h3 className='section-title'>Recent Work</h3>
             <dl className='resume-detail'>
               <dt><b>Washington Post Presidential Elections</b></dt>
               <dd>
                 <ul>
                   <li><em>Project lead, front-end build</em></li>
-                  <li>Launched dynamic, compelling coverage to millions of newswatchers on election day 2016 with no major hiccups.</li>
+                  <li>Launched dynamic, compelling coverage to millions of newswatchers on election day 2016.</li>
                   <li>Oversaw development of data pipeline and API capable of handling enormous traffic spikes.</li>
                   <li>Built reusable, responsive visual components integrated seamlessly into <a href='https://www.washingtonpost.com' target='_blank'>washingtonpost.com</a>.</li>
-                  <li>Leveraged and extended MapboxGL.js to render custom projections and real-time data.</li>
+                  <li>Leveraged and extended Mapbox GL JS to render custom projections and real-time data.</li>
                 </ul>
               </dd>
 
@@ -67,17 +67,73 @@ class Resume extends React.Component {
               <dd>
                 <ul>
                   <li><em>Project lead, infrastructure, front-end/back-end build</em></li>
-                  <li>Built platform to modernize data management practices at the world's largest humanitarian organization.</li>
+                  <li>Launched platform to modernize data management practices at the largest humanitarian organization.</li>
+                  <li>Designed and deployed cloud-native infrastructure architecture on Microsoft Azure.</li>
+                  <li>Conducted extensive in-person user research to tailor front-end functionality.</li>
+                </ul>
+              </dd>
+
+              <dt><b>Urban Change Explorer</b></dt>
+              <dd>
+                <ul>
+                  <li><em>Project lead, infrastructure, front-end build</em></li>
+                  <li>Created an urban intelligence tool for city planners to track urban change using satellite data.</li>
+                  <li>Developed map interface to evaluate change detection outputs against satellite and drone imagery.</li>
+                  <li>Built extensible <a href='https://github.com/mapbox/tippecanoe' target='_blank'>tippecanoe</a>-based vector tile pipeline capable of fast, incremental processing.</li>
+                </ul>
+              </dd>
+
+              <dt><b>OpenRoads</b></dt>
+              <dd>
+                <ul>
+                  <li><em>Front-end/back-end build</em></li>
+                  <li>Deployed OpenStreetMap (OSM) stack for collaborative road asset management in the Philippines.</li>
+                  <li>Built and released <a href='https://github.com/developmentseed/macrocosm' target='_blank'>Macrocosm</a>, a Node.js port of the OSM Rails API v0.6.</li>
+                </ul>
+              </dd>
+
+              <dt><b>Famine Early Warning Systems Network (FEWSNET)</b></dt>
+              <dd>
+                <ul>
+                  <li><em>Project lead</em></li>
+                  <li>Consulted to improve code quality, documentation, and development infrastructure.</li>
+                  <li>Implemented continuous integration, end-to-end integration test suite using <a href='https://www.cypress.io/' target='_blank'>cypress</a>.</li>
+                </ul>
+              </dd>
+
+              <dt><b>Cumulus Dashboard</b></dt>
+              <dd>
+                <ul>
+                  <li><em>Front-end build</em></li>
+                  <li>Built the front-end interface to a cloud-native satellite imagery pipeline for NASA.</li>
+                </ul>
+              </dd>
+
+              <dt><b>Obama Whitehouse Tweets</b></dt>
+              <dd>
+                <ul>
+                  <li>Rendered a <a href='http://lieu.io/whitehouse-tweet-topics/' target='_blank'>k-means clustering of Obama tweets</a> at the invitation of the White House press office.</li>
                 </ul>
               </dd>
             </dl>
           </section>
 
           <section className='section section-resume' id='misc'>
-            <h3 className='section-title'>Misc</h3>
+            <h3 className='section-title'>Misc.</h3>
             <dl className='resume-detail'>
-              <dt><strong>Bachelors of Arts, UCLA 2009</strong></dt>
-              <dd>My grades were ok</dd>
+              <dt><strong>Public Appearances</strong></dt>
+              <dd>
+                <ul>
+                  <li>I've spoken at a number of conferences, mostly relating to journalism, open data, and geospatial technology.</li>
+                  <li>Most recently, I presented my work on <a href='http://lieu.io/talks/2018-foss4g' target='_blank'>tracking urban change</a> at FOSS4G in Tanzania.</li>
+                </ul>
+              </dd>
+
+              <dt><strong>Lifestyle Choices</strong></dt>
+              <dd>I live and work in San Francisco, CA.</dd>
+
+              <dt><strong>Education</strong></dt>
+              <dd>I obtained a Bachelor's of Arts from UCLA in 2009. My grades were ok.</dd>
             </dl>
           </section>
         </main>
--- app/pages/work.js
+++ app/pages/work.js
@@ -24,7 +24,8 @@ class Work extends React.Component {
           <section className='section'>
             <p>I'm currently building a platform to dedupe relief efforts in the wake of a disaster by allowing crises responders to share actionable data and analysis. Situation reports as we know them today are hard to follow, and are out of date before they're published. I'm working with a group called <a target='_blank' href='https://nethope.org/'>NetHope</a> to do this better.</p>
             <p>(If you work with situation reports, I'd love to hear from you. You can also follow the work on <a href='https://github.com/developmentseed/nethope-sitrep-frontend' target='_blank'>Github</a>.)</p>
-            <p>In the past few years I've led software projects in partnership with the Washington Post, various departments in the World Bank, and the Federation of Red Cross Societies (IFRC). You can find my resume <a href={link('resume')} title='Resume'>here</a>.</p>
+            <p>In the past few years I've led software projects in partnership with the Washington Post, various departments in the World Bank, and the Federation of Red Cross Societies (IFRC).</p>
+            <p><a href={link('resume')} title='Resume'>You can find my resume here</a>.</p>
           </section>
         </main>
       </React.Fragment>
