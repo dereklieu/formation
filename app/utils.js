3aff59a6bc17fa3c8efbf100fe661592e90fe816
Derek Lieu <dl@dereklieu.com>
2019-03-13 14:11:46 -0700

    Remove fixed home style, append index.html to make lines work

--- app/utils.js
+++ app/utils.js
@@ -1,8 +1,9 @@
 'use strict'
 const slugify = require('slugify')
 const url = require('url')
+const path = require('path')
 const { baseUrl } = require('./config')
 
 export function link (displayName) {
-  return url.resolve(baseUrl, slugify(displayName))
+  return url.resolve(baseUrl, path.join(slugify(displayName), 'index.html'))
 }
