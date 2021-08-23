21a0c7844a20762a44c8115d8226176603289482
Derek Lieu <derek.lieu@mapbox.com>
2021-08-22 21:54:30 -0700

    Change work to builds

--- app/components/app.js
+++ app/components/app.js
@@ -4,18 +4,18 @@ const { StaticRouter, Route } = require('react-router-dom')
 
 const { paths } = require('../constants')
 const Home = require('../pages/home')
-const Work = require('../pages/work')
+const Builds = require('../pages/builds')
 const About = require('../pages/about')
 
 class App extends React.Component {
   render () {
     const { path } = this.props
     const router = (
-      <StaticRouter location={path} context={{}}>
+      <StaticRouter location={path}>
         <Route exact path={paths.home}
           render={(props) => <Home {...props} {...this.props} />}
         />
-        <Route exact path={paths.builds} component={Work} />
+        <Route exact path={paths.builds} component={Builds} />
         <Route exact path={paths.about} component={About} />
       </StaticRouter>
     )
